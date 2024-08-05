4747a9589be3f1fa73698cf7c8d2f91d4b86975f
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-05 13:16:46 +0930

    [error-handling]: Added better error handling.

--- server-actions/recaptcha.ts
+++ server-actions/recaptcha.ts
@@ -10,12 +10,30 @@ interface ReCaptcha {
 export async function getCaptchaValidity(key: string): Promise<boolean> {
   const captchaSecret = process.env.CAPTCHA_SECRET;
   const captchaUrl = process.env.CAPTCHA_URL;
+
+  if (!captchaSecret) {
+    throw new Error('CAPTCHA_SECRET is not defined');
+  }
+
+  if (!captchaUrl) {
+    throw new Error('CAPTCHA_URL is not defined');
+  }
+
   const response = await fetch(
     `${captchaUrl}?secret=${captchaSecret}&response=${key}`,
     {
       method: "POST",
     }
   );
-  const responseData: ReCaptcha = await response.json();
-  return responseData?.success;
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`);
+  }
+
+  try {
+    const responseData: ReCaptcha = await response.json();
+    return responseData.success;
+  } catch (error) {
+    throw new Error(`Failed to parse JSON: ${error}`);
+  }
 }
