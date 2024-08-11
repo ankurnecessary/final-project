129722238811c6bbab979d32d9055887bf6df2b6
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-11 12:42:41 +0930

    [story-book]: Expanded controls description in "Controls" section.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,6 +5,7 @@ import {INITIAL_VIEWPORTS} from "@storybook/addon-viewport";
 const preview: Preview = {
   parameters: {
     controls: {
+      expanded: true,
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
