d8dae1d3ad7da9710f86ed4a1ed2f9df6f4048a7
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-14 13:13:15 +0930

    [custom-ui]: Added 2 more variants of <Alert>. "warning" and "primary".

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -11,6 +11,10 @@ const alertVariants = cva(
         default: "bg-background text-foreground",
         destructive:
           "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+        warning:
+          "border-warning/50 text-warning dark:border-warning [&>svg]:text-warning",
+        primary:
+          "border-primary/50 text-primary dark:border-primary [&>svg]:text-primary",
       },
     },
     defaultVariants: {
