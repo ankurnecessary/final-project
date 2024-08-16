9366304d8b9e397b2e91097024b9cff00e63c270
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-16 11:28:42 +0930

    [story-book]: Added a story for signup page which includes integration tests too.

---
+++ components/register/register.stories.tsx
@@ -0,0 +1,82 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent, expect, waitFor, screen } from '@storybook/test';
+
+import Register from './register';
+
+const meta = {
+  title: 'Component/Register',
+  component: Register,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Register>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const signupButton = canvas.getByRole('button', { name: /^Sign up$/i });
+    await expect(signupButton).toBeInTheDocument();
+
+    // 👇 Simulate interactions with the signup button
+    await userEvent.click(signupButton);
+
+    
+    // 👇 Assert Name validation message
+    await waitFor(() => expect(screen.queryByText('Name is required')).toBeInTheDocument());
+
+    // 👇 Assert Email validation message
+    await waitFor(() => expect(screen.queryByText('Email is required')).toBeInTheDocument());
+
+    // 👇 Assert Password validation message
+    await waitFor(() => expect(screen.queryByText('Password is required')).toBeInTheDocument());
+
+    // 👇 Assert Confirm Password validation message
+    await waitFor(() => expect(screen.queryByText('Confirm Password is required')).toBeInTheDocument());
+
+    // 👇 Assert ReCAPTCHA validation message
+    await waitFor(() => expect(screen.queryByText('ReCAPTCHA is required')).toBeInTheDocument());
+
+    // 👇 Simulate interaction with the Name field
+    await userEvent.type(canvas.getByTestId('name'), 'Hello');
+
+    // 👇 Assert Name validation message not there
+    await waitFor(() => expect(screen.queryByText('Name is required')).not.toBeInTheDocument());
+
+    // 👇 Simulate interaction with the email field
+    await userEvent.type(canvas.getByTestId('email'), 'emailprovider.com');
+
+    // 👇 Assert Invalid Email validation message
+    await waitFor(() => expect(screen.queryByText('Invalid email address')).toBeInTheDocument());
+
+    // 👇 Simulate interactions with the email field
+    await userEvent.clear(canvas.getByTestId('email'));
+    await userEvent.type(canvas.getByTestId('email'), 'email@provider.com');
+
+    // 👇 Assert Email validation message not there
+    await waitFor(() => expect(screen.queryByText('Email is required')).not.toBeInTheDocument());
+
+    // 👇 Simulate interaction with the password field
+    await userEvent.type(canvas.getByTestId('password'), '123456');
+
+    // 👇 Assert Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Password is required')).not.toBeInTheDocument());
+
+    // 👇 Simulate interaction with the Confirm Password field
+    await userEvent.type(canvas.getByTestId('confirm-password'), '12345');
+
+    // 👇 Assert Confirm Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Passwords must match')).toBeInTheDocument());
+
+    // 👇 Simulate interactions with the Confirm Password field
+    await userEvent.clear(canvas.getByTestId('confirm-password'));
+    await userEvent.type(canvas.getByTestId('confirm-password'), '123456');
+
+    // 👇 Assert Confirm Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Confirm Password is required')).not.toBeInTheDocument());
+    await waitFor(() => expect(screen.queryByText('Passwords must match')).not.toBeInTheDocument());
+   
+  },
+};
