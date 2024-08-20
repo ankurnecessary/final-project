c09de947b928624295d0869430c6e8008d6be6b0
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-20 12:45:13 +0930

    [story-book][sign-up]: Added a interaction test cases where all the probabilities of the "password" field's validation errors are tested.

--- components/register/register.stories.tsx
+++ components/register/register.stories.tsx
@@ -23,7 +23,6 @@ export const Default: Story = {
     // 👇 Simulate interactions with the signup button
     await userEvent.click(signupButton);
 
-    
     // 👇 Assert Name validation message
     await waitFor(() => expect(screen.queryByText('Name is required')).toBeInTheDocument());
 
@@ -59,20 +58,50 @@ export const Default: Story = {
     await waitFor(() => expect(screen.queryByText('Email is required')).not.toBeInTheDocument());
 
     // 👇 Simulate interaction with the password field
-    await userEvent.type(canvas.getByTestId('password'), '123456');
+    await userEvent.clear(canvas.getByTestId('password'));
+    await userEvent.type(canvas.getByTestId('password'), 'h');
 
     // 👇 Assert Password validation message not there
-    await waitFor(() => expect(screen.queryByText('Password is required')).not.toBeInTheDocument());
+    await waitFor(() => expect(screen.queryByText('At least 8 characters')).toBeInTheDocument());
+
+    // 👇 Simulate interaction with the password field
+    await userEvent.clear(canvas.getByTestId('password'));
+    await userEvent.type(canvas.getByTestId('password'), 'hellohel');
+
+    // 👇 Assert Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Password must include at least 1 upper case letter.')).toBeInTheDocument());
+
+    // 👇 Simulate interaction with the password field
+    await userEvent.clear(canvas.getByTestId('password'));
+    await userEvent.type(canvas.getByTestId('password'), 'Hellohel');
+
+    // 👇 Assert Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Password must include at least 1 number.')).toBeInTheDocument());
+
+    // 👇 Simulate interaction with the password field
+    await userEvent.clear(canvas.getByTestId('password'));
+    await userEvent.type(canvas.getByTestId('password'), 'Hellohe1');
+
+    // 👇 Assert Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Password must include at least 1 special character.')).toBeInTheDocument());
+    
+    // 👇 Simulate interaction with the password field
+    await userEvent.clear(canvas.getByTestId('password'));
+    await userEvent.type(canvas.getByTestId('password'), 'Hello@he1');
+
+    // 👇 Assert Password validation message not there
+    await waitFor(() => expect(screen.queryByText('Password must include at least 1 special character.')).not.toBeInTheDocument());
 
     // 👇 Simulate interaction with the Confirm Password field
-    await userEvent.type(canvas.getByTestId('confirm-password'), '12345');
+    await userEvent.clear(canvas.getByTestId('confirm-password'));
+    await userEvent.type(canvas.getByTestId('confirm-password'), 'Hello@he12');
 
     // 👇 Assert Confirm Password validation message not there
     await waitFor(() => expect(screen.queryByText('Passwords must match')).toBeInTheDocument());
 
     // 👇 Simulate interactions with the Confirm Password field
     await userEvent.clear(canvas.getByTestId('confirm-password'));
-    await userEvent.type(canvas.getByTestId('confirm-password'), '123456');
+    await userEvent.type(canvas.getByTestId('confirm-password'), 'Hello@he1');
 
     // 👇 Assert Confirm Password validation message not there
     await waitFor(() => expect(screen.queryByText('Confirm Password is required')).not.toBeInTheDocument());
