d5fb1956d5cfbdb4c34ea0e751d7a1f36b98b106
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-26 21:39:16 +0930

    [login][refactor]: Took out extra HTML from login.tsx into page.tsx as we need to incorporate social login buttons which need to be placed in a server component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,42 @@
+import GithubSigninButton from "@/components/login/github-signin-button";
 import Login from "@/components/login/login";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <Login />
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <div className="max-w-md w-full space-y-8 md:border md:rounded-lg md:shadow-lg p-12">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold tracking-tight">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/register"
+              className="font-medium text-primary hover:underline"
+              prefetch={false}
+            >
+              Register
+            </Link>
+          </p>
+        </div>
+        <Login />
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t border-muted" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or sign in with
+            </span>
+          </div>
+        </div>
+        <Button variant="outline" className="w-full">
+          Sign in with Google
+        </Button>
+      </div>
+    </div>
   );
 }
--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import * as Yup from "yup";
 import { Formik, Form, ErrorMessage } from "formik";
 import { Label } from "../ui/label";
@@ -22,119 +21,89 @@ const Login = () => {
   const [captchaError, setCaptchaError] = useState<boolean>(false);
   const recaptchaRef = useRef<ReCAPTCHA>(null);
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
-      <div className="max-w-md w-full space-y-8 md:border md:rounded-lg md:shadow-lg p-12">
-        <div className="text-center">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Sign in to your account
-          </h2>
-          <p className="mt-2 text-muted-foreground">
-            Don&apos;t have an account?{" "}
-            <Link
-              href="/register"
-              className="font-medium text-primary hover:underline"
-              prefetch={false}
-            >
-              Register
-            </Link>
-          </p>
-        </div>
-        {captchaError && (
-          <ErrorAlert
-            heading="Error"
-            description="Error with captcha. Please try again."
-          />
+    <>
+      {captchaError && (
+        <ErrorAlert
+          heading="Error"
+          description="Error with captcha. Please try again."
+        />
+      )}
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          recaptcha: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { setSubmitting }) => {
+          console.log("Form data", values);
+          const isCaptchaValid = await getCaptchaValidity(values.recaptcha);
+          if (!isCaptchaValid) {
+            setCaptchaError(true);
+            recaptchaRef.current?.reset();
+            return;
+          }
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting, setFieldValue }) => (
+          <Form className="space-y-6" noValidate>
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="true"
+                data-testid="email"
+                required
+                className="mt-1"
+                placeholder="Enter your email"
+              />
+              <ErrorMessage
+                name="email"
+                component="span"
+                className="text-red-600"
+              />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                data-testid="password"
+                name="password"
+                type="password"
+                autoComplete="true"
+                required
+                className="mt-1"
+                placeholder="Enter your password"
+              />
+              <ErrorMessage
+                name="password"
+                component="span"
+                className="text-red-600"
+              />
+            </div>
+            <div>
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey="6Lfd9xkqAAAAACTLpOgZd-iIuwV6dTxPZdrctvEI"
+                onChange={(value) => setFieldValue("recaptcha", value)}
+                title="reCAPTCHA-i"
+              />
+              <ErrorMessage
+                name="recaptcha"
+                component="span"
+                className="text-red-600"
+              />
+            </div>
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              Sign in
+            </Button>
+          </Form>
         )}
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-            recaptcha: "",
-          }}
-          validationSchema={validationSchema}
-          onSubmit={async (values, { setSubmitting }) => {
-            console.log("Form data", values);
-            const isCaptchaValid = await getCaptchaValidity(values.recaptcha);
-            if (!isCaptchaValid) {
-              setCaptchaError(true);
-              recaptchaRef.current?.reset();
-              return;
-            }
-            setSubmitting(false);
-          }}
-        >
-          {({ isSubmitting, setFieldValue }) => (
-            <Form className="space-y-6" noValidate>
-              <div>
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="true"
-                  data-testid="email"
-                  required
-                  className="mt-1"
-                  placeholder="Enter your email"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="span"
-                  className="text-red-600"
-                />
-              </div>
-              <div>
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  id="password"
-                  data-testid="password"
-                  name="password"
-                  type="password"
-                  autoComplete="true"
-                  required
-                  className="mt-1"
-                  placeholder="Enter your password"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="span"
-                  className="text-red-600"
-                />
-              </div>
-              <div>
-                <ReCAPTCHA
-                  ref={recaptchaRef}
-                  sitekey="6Lfd9xkqAAAAACTLpOgZd-iIuwV6dTxPZdrctvEI"
-                  onChange={(value) => setFieldValue("recaptcha", value)}
-                  title="reCAPTCHA-i"
-                />
-                <ErrorMessage
-                  name="recaptcha"
-                  component="span"
-                  className="text-red-600"
-                />
-              </div>
-              <Button type="submit" className="w-full" disabled={isSubmitting}>
-                Sign in
-              </Button>
-            </Form>
-          )}
-        </Formik>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t border-muted" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or sign in with
-            </span>
-          </div>
-        </div>
-        <Button variant="outline" className="w-full">
-          Sign in with Google
-        </Button>
-      </div>
-    </div>
+      </Formik>
+    </>
   );
 };
 
