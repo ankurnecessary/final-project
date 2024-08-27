e7a444816e385b7a99a350dd03b099beb7e139ff
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-27 21:23:28 +0930

    [social-auth]: Printed session details on the chat page.

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,13 @@
+import { auth } from '@/authentication/auth'
+import SessionData from '@/components/sessionData/session-data'
 import React from 'react'
 
-const ChatPage = () => {
+const ChatPage = async () => {
+  const session = await auth()
   return (
     <div>
       <h1>Chat page</h1>
+      <SessionData session={session} />
     </div>
   )
 }
