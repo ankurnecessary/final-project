3732e384120d17b6ffbc2f42ba14fdc95ba4bbf3
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-29 21:30:12 +0930

    [auth-redirection]: Added redirection to login page if the login session is not valid.

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,14 @@
 import { auth } from '@/authentication/auth'
 import SessionData from '@/components/sessionData/session-data'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const ChatPage = async () => {
   const session = await auth()
+
+  // Redirecting to login page if session is invalid
+  if(!session) redirect('/login');
+
   return (
     <div>
       <h1>Chat page</h1>
