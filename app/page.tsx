cffbe7d65b867a9df1b972a675142c727d7d1a8b
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-29 21:24:52 +0930

    [home-page]: Removed all the code related to login from the home page. Currently it is blank.

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,9 @@
 import GithubSigninButton from "@/components/Login/github-signin-button";
 import Login from "@/components/Login/login";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
-      <div className="max-w-md w-full space-y-8 md:border md:rounded-lg md:shadow-lg p-12">
-        <div className="text-center">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Sign in to your account
-          </h2>
-          <p className="mt-2 text-muted-foreground">
-            Don&apos;t have an account?{" "}
-            <Link
-              href="/register"
-              className="font-medium text-primary hover:underline"
-              prefetch={false}
-            >
-              Register
-            </Link>
-          </p>
-        </div>
-        <Login />
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t border-muted" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or sign in with
-            </span>
-          </div>
-        </div>
-        <GithubSigninButton provider="github" />
-      </div>
-    </div>
+    <h1>Home page</h1>
   );
 }
