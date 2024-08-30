8d7864b9a79a9e7709a21a55809e29f9e9c77325
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-31 08:05:35 +0930

    [social-auth]: Now, if user will try to access sign-up page when it is already logged in then he will be routed to home page.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,13 @@
+import { auth } from "@/authentication/auth";
 import Register from "@/components/Register/register";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  // Redirecting to home page if session is valid
+  if (session) redirect("/");
+  
   return (
     <Register />
   );
