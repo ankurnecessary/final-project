ed586da27f7763b7b0c51ad70c7ea6b77068ce2f
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-08-31 07:47:29 +0930

    [social-auth]: Removed unnecessary code for github login configuration.

--- authentication/options.ts
+++ authentication/options.ts
@@ -1,22 +1,9 @@
-import GitHubProvider, { GitHubProfile } from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const options = {
   providers: [
-    GitHubProvider({
-      // profile function helps in setting the role for a profile
-      profile(profile: GitHubProfile) {
-
-        let userRole = "Github user";
-        if (profile.email === "ankurnecessary@gmail.com") {
-          userRole = "admin";
-        }
-
-        return { ...profile, id: profile.id.toString(), role: userRole };
-      },
-      clientId: process.env.AUTH_GITHUB_ID || '',
-      clientSecret: process.env.AUTH_GITHUB_SECRET || '',
-    }),
+    GitHub,
     Google 
   ],
 };
