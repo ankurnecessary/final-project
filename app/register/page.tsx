4094673eaaf6ae309d84ecae05e74ad371573399
Ankur Gupta <5038755+ankurnecessary@users.noreply.github.com>
2024-09-01 08:38:39 +0930

    [social-auth]: Added social auth buttons on registration page too.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/authentication/auth";
+import SocialSigninButtonGroup from "@/components/Login/social-signin-button-group";
 import Register from "@/components/Register/register";
 import { redirect } from "next/navigation";
 
@@ -7,8 +8,19 @@ export default async function Home() {
 
   // Redirecting to home page if session is valid
   if (session) redirect("/");
-  
+
   return (
-    <Register />
+    <div className="flex items-center justify-center md:min-h-[calc(100vh-4rem)] bg-background">
+      <div className="md:max-w-xl w-full space-y-8 md:border md:rounded-lg md:shadow-lg p-12 my-5">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold tracking-tight">Welcome!</h2>
+          <p className="mt-2 text-muted-foreground">
+            Create your account to get started.
+          </p>
+        </div>
+        <Register />
+        <SocialSigninButtonGroup />
+      </div>
+    </div>
   );
 }
--- components/register/register.tsx
+++ components/register/register.tsx
@@ -38,154 +38,146 @@ const Register = () => {
   const [captchaError, setCaptchaError] = useState<boolean>(false);
   const recaptchaRef = useRef<ReCAPTCHA>(null);
   return (
-    <div className="flex items-center justify-center md:min-h-[calc(100vh-4rem)] bg-background">
-      <div className="md:max-w-xl w-full space-y-8 md:border md:rounded-lg md:shadow-lg p-12">
-        <div className="text-center">
-          <h2 className="text-3xl font-bold tracking-tight">Welcome!</h2>
-          <p className="mt-2 text-muted-foreground">
-            Create your account to get started.
-          </p>
-        </div>
-        {captchaError && (
-          <ErrorAlert
-            heading="Error"
-            description="Error with captcha. Please try again."
-          />
-        )}
-        <Formik
-          initialValues={{
-            name: "",
-            email: "",
-            password: "",
-            "confirm-password": "",
-            recaptcha: "",
-          }}
-          validationSchema={validationSchema}
-          onSubmit={async (values, { setSubmitting }) => {
-            console.log("Form data", values);
-            const isCaptchaValid = await getCaptchaValidity(values.recaptcha);
-            if (!isCaptchaValid) {
-              setCaptchaError(true);
-              recaptchaRef.current?.reset();
-              return;
-            }
-            setSubmitting(false);
-          }}
-        >
-          {({ isSubmitting, setFieldValue }) => (
-            <Form noValidate>
-              <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
-                <div>
-                  <Label htmlFor="name" className="flex justify-between">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    data-testid="name"
-                    name="name"
-                    type="text"
-                    autoComplete="true"
-                    required
-                    className="mt-1"
-                    placeholder="Enter your name"
-                  />
-                  <ErrorMessage
-                    name="name"
-                    component="span"
-                    className="text-red-600"
-                  />
-                </div>
-                <div>
-                  <Label htmlFor="email" className="flex justify-between">
-                    Email
-                  </Label>
-                  <Input
-                    id="email"
-                    data-testid="email"
-                    name="email"
-                    type="email"
-                    autoComplete="true"
-                    required
-                    className="mt-1"
-                    placeholder="Enter your email"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="span"
-                    className="text-red-600"
-                  />
-                </div>
-                <div>
-                  <Label htmlFor="password" className="flex">
-                    Password
-                    <InfoPopover label="Password information" contentOffset={6}>
-                        <PasswordInfo></PasswordInfo>
-                    </InfoPopover>
-                  </Label>
-                  <Input
-                    id="password"
-                    data-testid="password"
-                    name="password"
-                    type="password"
-                    autoComplete="true"
-                    required
-                    className="mt-1"
-                    placeholder="Enter your password"
-                  />
-                  <ErrorMessage
-                    name="password"
-                    component="span"
-                    className="text-red-600"
-                  />
-                </div>
-                <div>
-                  <Label
-                    htmlFor="confirm-password"
-                    className="flex justify-between"
-                  >
-                    Confirm Password
-                  </Label>
-                  <Input
-                    id="confirm-password"
-                    data-testid="confirm-password"
-                    name="confirm-password"
-                    type="password"
-                    autoComplete="true"
-                    required
-                    className="mt-1"
-                    placeholder="Enter your password again"
-                  />
-                  <ErrorMessage
-                    name="confirm-password"
-                    component="span"
-                    className="text-red-600"
-                  />
-                </div>
-              </div>
-              <div className="mt-8">
-                <ReCAPTCHA
-                  ref={recaptchaRef}
-                  sitekey="6Lfd9xkqAAAAACTLpOgZd-iIuwV6dTxPZdrctvEI"
-                  onChange={(value) => setFieldValue("recaptcha", value)}
+    <>
+      {captchaError && (
+        <ErrorAlert
+          heading="Error"
+          description="Error with captcha. Please try again."
+        />
+      )}
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          "confirm-password": "",
+          recaptcha: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={async (values, { setSubmitting }) => {
+          console.log("Form data", values);
+          const isCaptchaValid = await getCaptchaValidity(values.recaptcha);
+          if (!isCaptchaValid) {
+            setCaptchaError(true);
+            recaptchaRef.current?.reset();
+            return;
+          }
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting, setFieldValue }) => (
+          <Form noValidate>
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
+              <div>
+                <Label htmlFor="name" className="flex justify-between">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  data-testid="name"
+                  name="name"
+                  type="text"
+                  autoComplete="true"
+                  required
+                  className="mt-1"
+                  placeholder="Enter your name"
                 />
                 <ErrorMessage
-                  name="recaptcha"
+                  name="name"
                   component="span"
                   className="text-red-600"
                 />
               </div>
-              <Button
-                type="submit"
-                className="w-full mt-8"
-                disabled={isSubmitting}
-              >
-                Sign up
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </div>
+              <div>
+                <Label htmlFor="email" className="flex justify-between">
+                  Email
+                </Label>
+                <Input
+                  id="email"
+                  data-testid="email"
+                  name="email"
+                  type="email"
+                  autoComplete="true"
+                  required
+                  className="mt-1"
+                  placeholder="Enter your email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className="text-red-600"
+                />
+              </div>
+              <div>
+                <Label htmlFor="password" className="flex">
+                  Password
+                  <InfoPopover label="Password information" contentOffset={6}>
+                    <PasswordInfo></PasswordInfo>
+                  </InfoPopover>
+                </Label>
+                <Input
+                  id="password"
+                  data-testid="password"
+                  name="password"
+                  type="password"
+                  autoComplete="true"
+                  required
+                  className="mt-1"
+                  placeholder="Enter your password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="span"
+                  className="text-red-600"
+                />
+              </div>
+              <div>
+                <Label
+                  htmlFor="confirm-password"
+                  className="flex justify-between"
+                >
+                  Confirm Password
+                </Label>
+                <Input
+                  id="confirm-password"
+                  data-testid="confirm-password"
+                  name="confirm-password"
+                  type="password"
+                  autoComplete="true"
+                  required
+                  className="mt-1"
+                  placeholder="Enter your password again"
+                />
+                <ErrorMessage
+                  name="confirm-password"
+                  component="span"
+                  className="text-red-600"
+                />
+              </div>
+            </div>
+            <div className="mt-8">
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey="6Lfd9xkqAAAAACTLpOgZd-iIuwV6dTxPZdrctvEI"
+                onChange={(value) => setFieldValue("recaptcha", value)}
+              />
+              <ErrorMessage
+                name="recaptcha"
+                component="span"
+                className="text-red-600"
+              />
+            </div>
+            <Button
+              type="submit"
+              className="w-full mt-8"
+              disabled={isSubmitting}
+            >
+              Sign up
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
